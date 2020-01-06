d53172b31d11d677f45e3a668f40174b1b963f81
Joshua Willigers <jwilligers@gmail.com>
2020-01-06 16:46:13 +1100

    Addition of Hatchbot example

--- 2020MainRobot/src/main/java/frc/robot/Robot.java
+++ 2020MainRobot/src/main/java/frc/robot/Robot.java
@@ -1,125 +1,125 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------*/                                            
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */                                            
+/* Open Source Software - may be modified and shared by FRC teams. The code   */                                            
+/* must be accompanied by the FIRST BSD license file in the root directory of */                                            
+/* the project.                                                               */                                            
+/*----------------------------------------------------------------------------*/                                            
 
-package frc.robot;
+package frc.robot;                                                                                    
 
-import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.subsystems.Climber;
-import frc.robot.subsystems.Drivetrain;
-/**import frc.robot.autonomous.PathWeaver;
-import frc.robot.autonomous.ViveAuto;
-import frc.robot.autonomous.ViveMeasurements;
-import frc.robot.driver.LEDController;
-import frc.robot.subsystem.*;
-import edu.wpi.first.wpilibj.networktables.NetworkTable;*/
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.PowerDistributionPanel;
-
-
-public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  double curr_angle, target_angle;
-  public Joystick joystick = new Joystick(1);
-  public XboxController xboxController = new XboxController(2);
-  public Climber climber = new Climber();
-  public PowerCellIntake PowerCellIntake = new PowerCellIntake();
-  public Drivetrain drivetrain = new Drivetrain();
-  public VisionController visionController = new VisionController();
-  public LEDController ledController = new LEDController();
-  Timer matchTimer = new Timer();
-  NetworkTable table;
-  String autoMode;
-
-  public PowerDistributionPanel pdp;
-  boolean invertedDrive;
-  double speedControl;
-  boolean debug;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+import edu.wpi.first.wpilibj.*;                                                                                    
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;                                                                                    
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;                                                                                    
+import frc.robot.subsystems.Climber;                                                                                    
+import frc.robot.subsystems.Drivetrain;                                                                                    
+/**import frc.robot.autonomous.PathWeaver;                                                                                    
+import frc.robot.autonomous.ViveAuto;                                                                                    
+import frc.robot.autonomous.ViveMeasurements;                                                                                    
+import frc.robot.driver.LEDController;                                                                                    
+import frc.robot.subsystem.*;                                                                                    
+import edu.wpi.first.wpilibj.networktables.NetworkTable;*/                                                                                    
+import edu.wpi.first.cameraserver.CameraServer;                                                                                    
+import edu.wpi.first.networktables.NetworkTable;                                                                                    
+import edu.wpi.first.wpilibj.Timer;                                                                                    
+import edu.wpi.first.wpilibj.PowerDistributionPanel;                                                                                    
 
 
-  @Override
-  public void robotInit() {
-    debug = true;
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    invertedDrive = false;
-    speedControl = 0.5;
+public class Robot extends TimedRobot {                                                                                    
+  private static final String kDefaultAuto = "Default";                                                                                    
+  private static final String kCustomAuto = "My Auto";                                                                                    
+  private String m_autoSelected;                                                                                    
+  double curr_angle, target_angle;                                                                                    
+  public Joystick joystick = new Joystick(1);                                                                                    
+  public XboxController xboxController = new XboxController(2);                                                                                    
+  public Climber climber = new Climber();                                                                                    
+  public PowerCellIntake PowerCellIntake = new PowerCellIntake();                                                                                    
+  public Drivetrain drivetrain = new Drivetrain();                                                                                    
+  public VisionController visionController = new VisionController();                                            
+  public LEDController ledController = new LEDController();                                            
+  Timer matchTimer = new Timer();                                            
+  NetworkTable table;                                            
+  String autoMode;                                            
 
-   /* table = NetworkTable.getTable("GRIP/myContoursReport");*/
-    CameraServer.getInstance().startAutomaticCapture();
-  }
-
-  @Override
-  public void robotPeriodic() {
-  }
-
- /* @Override
-  public void autonomousInit() {
-    matchTimer.start();
-    drivetrain.resetEncoders();
-    autoMode = m_chooser.getSelected();
-
-    if (autoMode == "Pathfinder")
-    {
-      Object pathweaver;
-      pathweaver.wait(drivetrain);
-    }
-    else
-    {
-
-    }
-    strategy.reset();
-  }
+  public PowerDistributionPanel pdp;                                            
+  boolean invertedDrive;                                            
+  double speedControl;                                            
+  boolean debug;                                            
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();                                            
 
 
-  @Override
-  public void autonomousPeriodic() {
-    //pathweaver.followPath(drivetrain);
-    strategy.run(this);
-    updateDashboards();
-  }
+  @Override                                            
+  public void robotInit() {                                            
+    debug = true;                                            
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);                                            
+    invertedDrive = false;                                            
+    speedControl = 0.5;                                            
 
-  @Override
-  public void teleopInit() {
-    climber.recoverCarrige();
-    panelintake.getPannel();
-    climber.stopVacuum();
-    drivetrain.resetEncoders();
-  }
-*/
-  @Override
-  public void teleopPeriodic() {
-    drive();
-    double[] defaultValue = new double[2];
+   /* table = NetworkTable.getTable("GRIP/myContoursReport");*/                                            
+    CameraServer.getInstance().startAutomaticCapture();                                            
+  }                                            
 
-    /*double[] distances = table.getNumberArray("centreX", defaultValue);
-    SmartDashboard.putNumber("Distance 0", distances[0]);*/
-
-    visionController.update();
-    updateDashboards();
-  }
-
-  private void drive() {
-  }
-
-  @Override
-  public void testPeriodic() {
-  }
-  public void updateDashboards()
-  {
-    PowerCellIntake.updateDashboard(true);
-    //climber.updateDashboard(debug);
-    drivetrain.updateDashboard(debug);
-    //visionController.updateDashboard(debug);
-    //panelintake.updateDashboard(debug);
-  }
-}
+  @Override                                            
+  public void robotPeriodic() {                                            
+  }                                                 
+    
+ /* @Override                                                 
+  public void autonomousInit() {                                                                                            
+    matchTimer.start();                                                                                            
+    drivetrain.resetEncoders();                                                                                            
+    autoMode = m_chooser.getSelected();                                                                                            
+    
+    if (autoMode == "Pathfinder")                                                                                            
+    {                                                                                            
+      Object pathweaver;                                                                                            
+      pathweaver.wait(drivetrain);                                                                                            
+    }                                                                                            
+    else                                                                                            
+    {                                                                                            
+    
+    }                                                                                            
+    strategy.reset();                                                                                            
+  }                                                                                                                                              
+                                                                                                  
+                                                                                                  
+  @Override                                                                                                                                         
+  public void autonomousPeriodic() {                                                                                                                                         
+    //pathweaver.followPath(drivetrain);                                                                                                                                         
+    strategy.run(this);                                                                                                                                         
+    updateDashboards();                                                                                                                                         
+  }                                                                                                                                         
+                                                                                              
+  @Override                                                                                                                                         
+  public void teleopInit() {                                                                                                                                         
+    climber.recoverCarrige();                                                                                                                                         
+    panelintake.getPannel();                                                                                                                                         
+    climber.stopVacuum();                                                                                                                                         
+    drivetrain.resetEncoders();                                                                                                                                         
+  }                                                                                                                                         
+*/                                                                                                                                         
+  @Override                                                                                                                                         
+  public void teleopPeriodic() {                                                                                                                                         
+    drive();                                                                                                                                         
+    double[] defaultValue = new double[2];                                                                                                                                         
+                                                                                              
+    /*double[] distances = table.getNumberArray("centreX", defaultValue);                                                                                                                                         
+    SmartDashboard.putNumber("Distance 0", distances[0]);*/                                                                                                                                         
+                                                                                                                                                                                     
+    visionController.update();                                                                                                                                         
+    updateDashboards();                                                                                                                                         
+  }                                                                                                                                         
+                                                                                              
+  private void drive() {                                                                                                                                         
+  }                                                                                                                                         
+                                                                                              
+  @Override                                                                                                                                         
+  public void testPeriodic() {                                                                                                                                         
+  }                                                                                                                                         
+  public void updateDashboards()                                                                                                                                         
+  {                                                                                                                                         
+    PowerCellIntake.updateDashboard(true);                                                                                                                                         
+    //climber.updateDashboard(debug);                                                                                                                                         
+    drivetrain.updateDashboard(debug);                                                                                                                                         
+    //visionController.updateDashboard(debug);                                                                                                                                         
+    //panelintake.updateDashboard(debug);                                                                                                                                         
+  }                                           
+}                                           
