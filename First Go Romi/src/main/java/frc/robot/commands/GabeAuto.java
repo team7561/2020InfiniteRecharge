87f8d24a5057d849bb07199a67e3a2451aa4137e
Gabriel Hanich <gabriel.hanich1@gmail.com>
2021-03-21 15:48:15 +1100

    Adding First Go Romi

---
+++ First Go Romi/src/main/java/frc/robot/commands/GabeAuto.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class GabeAuto extends SequentialCommandGroup {
+  private final Drivetrain m_drivetrain;
+  /**
+   * Creates a new Autonomous Drive based on distance. This will drive out for a specified distance,
+   * turn around and drive back.
+   *
+   * @param drivetrain The drivetrain subsystem on which this command will run
+   */
+  public GabeAuto(Drivetrain drivetrain) {
+    m_drivetrain = drivetrain;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    m_drivetrain.setLeftSpeed(1.0);  
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+  
+
+
+}
